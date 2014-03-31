a695f04e5c265be1e369d7ee7bf1a109e7213e34
gooliver <dima.choban@gmail.com>
2014-03-31 19:39:33 +0300

    new services added, style.css added

--- central_park/services.py
+++ central_park/services.py
@@ -1,7 +1,8 @@
 from database import db_session
 from models import ParkingLot, ParkingPlace, PriceHistory, Payment
 from sqlalchemy import desc
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 # return current pricehistory_id for given parking_lot_id
 def get_current_pricehistory_id(current_parkinglot_id):
@@ -23,35 +24,62 @@ def get_current_tariff_matrix(current_parkinglot_id):
         return None
 
 
+# return time that shows when to leave parking place.
 def calculate_estimated_time(cost, lot_id):
     tariff = get_current_tariff_matrix(lot_id)
     tariff = parse_tariff_to_list(tariff)
     time_finish = time_start = datetime.now()
-    minutes = 60 - time_start.minute
-    cost_in_first_hour = minutes * tariff[time_start.hour] / 60
+    cost_in_first_hour = calculate_minutes_cost(tariff[time_start.hour], 60 - time_start.minute)
     if (cost_in_first_hour < cost):
         cost -= cost_in_first_hour
         hour = time_start.hour + 1
-        time_finish.hour += 1
-        time_finish.minute = 00
-        time_finish.second = 00
+        time_finish += timedelta(hours=1)
+        time_finish += timedelta(minutes=60-time_finish.minute)
+        time_finish += timedelta(minutes=60-time_finish.second)
         while cost > tariff[hour]:
             cost -= tariff[hour]
-            time_finish.hour += 1
+            time_finish += timedelta(hours=1)
             if hour < 23:
                 hour += 1
             else:
                 hour = 0
-                time_finish.day += 1
         else:
-            #minutes_in_last_hour = 60 * cost / tariff[hour]
             minutes_in_last_hour = calculate_estimated_time_in_last_hour(cost, tariff[hour])
-            time_finish.minute = minutes_in_last_hour
+            time_finish += timedelta(minutes=minutes_in_last_hour)
     else:
-        #minutes_in_last_hour = 60 * cost / tariff[time_start.hour]
         minutes_in_last_hour = calculate_estimated_time_in_last_hour(cost, tariff[time_start.hour])
-        time_finish.minute += minutes_in_last_hour
+        time_finish += timedelta(minutes=minutes_in_last_hour)
     return time_finish
+
+
+#return price of parking till marked time.
+def calculate_total_price(lot_id, time_finish):
+    tariff = get_current_tariff_matrix(lot_id)
+    tariff = parse_tariff_to_list(tariff)
+    time_start = datetime.now()
+    if (time_finish.hour == time_start.hour and time_finish.day == time_start.day):
+        return calculate_minutes_cost(time_finish.minute - time_start.minute, tariff[time_start.hour])
+    else:
+        cost = calculate_minutes_cost(tariff[time_start.hour], 60 - time_start.minute)
+        hour = time_start.hour + 1
+        time_start += timedelta(hours=1)
+        time_start += timedelta(minutes=60-time_start.minute)
+        time_start += timedelta(minutes=60-time_start.second)
+        while time_start.hour < time_finish.hour:
+            cost += tariff[hour]
+            time_start += timedelta(hours=1)
+            if hour < 23:
+                hour += 1
+            else:
+                hour = 0
+        else:
+            cost += calculate_minutes_cost(tariff[hour], time_finish.minute - time_start.minute)
+            return cost
+
+
+#some internal functions
+def calculate_minutes_cost(price_of_hour, minutes):
+    return minutes * price_of_hour / 60
 
 
 def calculate_estimated_time_in_last_hour(estimated_money, price_of_hour):
@@ -60,4 +88,3 @@ def calculate_estimated_time_in_last_hour(estimated_money, price_of_hour):
 
 def parse_tariff_to_list(tariff):
     return tuple([int(x) for x in tariff.split(';')])
-
